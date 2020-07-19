3331edab06a85c5725454640d34e70f6d6f64125
danmunyua <ndegwamunyua@gmail.com>
2020-07-19 12:41:55 +0300

    A shortcut to the start_requests method
    define a start_urls class attribute with a list of URLs

--- tutorial/spiders/quotes_spider.py
+++ tutorial/spiders/quotes_spider.py
@@ -1,16 +1,13 @@
-from scrapy import Request, Spider
+from scrapy import Spider
 
 
 class QuotesSpider(Spider):
     name = "quotes"
 
-    def start_requests(self):
-        urls = [
-            "http://quotes.toscrape.com/page/1/",
-            "http://quotes.toscrape.com/page/2/",
-        ]
-        for url in urls:
-            yield Request(url=url, callback=self.parse)
+    start_urls = [
+        "http://quotes.toscrape.com/page/1/",
+        "http://quotes.toscrape.com/page/2/",
+    ]
 
     def parse(self, response):
         page = response.url.split("/")[-2]
