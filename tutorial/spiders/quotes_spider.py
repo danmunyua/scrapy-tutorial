d574e9be965dc5c91c7032c0ff9ea9730485059b
danmunyua <ndegwamunyua@gmail.com>
2020-07-19 15:52:47 +0300

    Using spider arguments
    use -a option to provide arguments to your spiders, where theyre passed to Spiders __init__ method.

--- tutorial/spiders/quotes_spider.py
+++ tutorial/spiders/quotes_spider.py
@@ -1,20 +1,21 @@
-from scrapy import Spider
+from scrapy import Request, Spider
 
 
 class QuotesSpider(Spider):
     name = "quotes"
 
-    start_urls = [
-        "http://quotes.toscrape.com/page/1/",
-        "http://quotes.toscrape.com/page/2/",
-    ]
+    def start_requests(self):
+        url = "http://quotes.toscrape.com/"
+        tag = getattr(self, "tag", None)
+        if tag is not None:
+            url = url + "tag/" + tag
+        yield Request(url=url, callback=self.parse)
 
     def parse(self, response):
         for quote in response.css("div.quote"):
             yield {
                 "text": quote.css("span.text::text").get(),
                 "author": quote.css("small.author::text").get(),
-                "tags": quote.css("div.tags a.tag::text").getall(),
             }
         next_page = response.css("li.next a::attr(href)").get()
         if next_page is not None:
