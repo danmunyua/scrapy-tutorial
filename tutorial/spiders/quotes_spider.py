8a2b8b8ffea6787f58c17adb1bbe4518ebdcb821
danmunyua <ndegwamunyua@gmail.com>
2020-07-19 13:42:19 +0300

    Following links
    following all the links to the next page until it doesn’t find one – handy for crawling blogs, forums and other sites with pagination.

--- tutorial/spiders/quotes_spider.py
+++ tutorial/spiders/quotes_spider.py
@@ -1,4 +1,4 @@
-from scrapy import Spider
+from scrapy import Request, Spider
 
 
 class QuotesSpider(Spider):
@@ -16,3 +16,7 @@ class QuotesSpider(Spider):
                 "author": quote.css("small.author::text").get(),
                 "tags": quote.css("div.tags a.tag::text").getall(),
             }
+        next_page = response.css("li.next a::attr(href)").get()
+        if next_page is not None:
+            next_page = response.urljoin(next_page)
+            yield Request(url=next_page, callback=self.parse)
